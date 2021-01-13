7332554cf04439fab799f106330cb76a5ed7607e
qx103665 <2307499917@qq.com>
2021-01-13 20:17:25 +0800

    1.正则表达式匹配并获取错误消息的内容 2.获取所有的航班信息并显示出来

---
+++ src/cn/edu/hcnu/bean/Customer.java
@@ -0,0 +1,71 @@
+package cn.edu.hcnu.bean;
+
+import java.util.Date;
+import java.util.Set;
+
+public class Customer {
+
+    private String id;//没有业务意义
+    private String customerType;//旅客类型：成人、儿童、婴儿
+    private String name;//旅客姓名
+    private String phone;
+    private String cardId;
+    private Date birthDate;
+    private Set<Order> orderSet;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Set<Order> getOrderSet() {
+        return orderSet;
+    }
+
+    public void setOrderSet(Set<Order> orderSet) {
+        this.orderSet = orderSet;
+    }
+}
---
+++ src/cn/edu/hcnu/bean/Order.java
@@ -0,0 +1,97 @@
+package cn.edu.hcnu.bean;
+
+import java.util.Set;
+
+public class Order {
+
+    private String id;
+    private float jpjg;//机票价格
+    private float airPortTax;//机场税
+    private float ryf;//燃油费
+    private float hkzhx;//航空综合险
+    private float jptgx;//机票退改险
+    private float yhq;//优惠券
+    private float hszj;//含税总价
+    private Set<Customer> customerSet;
+    private Flight flight;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public float getJpjg() {
+        return jpjg;
+    }
+
+    public void setJpjg(float jpjg) {
+        this.jpjg = jpjg;
+    }
+
+    public float getAirPortTax() {
+        return airPortTax;
+    }
+
+    public void setAirPortTax(float airPortTax) {
+        this.airPortTax = airPortTax;
+    }
+
+    public float getRyf() {
+        return ryf;
+    }
+
+    public void setRyf(float ryf) {
+        this.ryf = ryf;
+    }
+
+    public float getHkzhx() {
+        return hkzhx;
+    }
+
+    public void setHkzhx(float hkzhx) {
+        this.hkzhx = hkzhx;
+    }
+
+    public float getJptgx() {
+        return jptgx;
+    }
+
+    public void setJptgx(float jptgx) {
+        this.jptgx = jptgx;
+    }
+
+    public float getYhq() {
+        return yhq;
+    }
+
+    public void setYhq(float yhq) {
+        this.yhq = yhq;
+    }
+
+    public float getHszj() {
+        return hszj;
+    }
+
+    public void setHszj(float hszj) {
+        this.hszj = hszj;
+    }
+
+    public Set<Customer> getCustomerSet() {
+        return customerSet;
+    }
+
+    public void setCustomerSet(Set<Customer> customerSet) {
+        this.customerSet = customerSet;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+}
